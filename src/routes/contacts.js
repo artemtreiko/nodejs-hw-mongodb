95a332a9b6797f1e1f8f06366961913ed85f86d7
artemtreiko <artemtreiko@gmail.com>
2025-10-12 12:44:58 +0300

    validation, middlewares

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -6,12 +6,18 @@ import {
   deleteContactController
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { isValidId } from '../middlewares/isValidId.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema, updateContactSchema } from '../validation/contacts.js';
 
 const router = Router();
 
 router.get('/', ctrlWrapper(getContactsController));
-router.get('/:contactId', ctrlWrapper(getContactByIdController));
+
+router.get('/:contactId', isValidId, ctrlWrapper(getContactByIdController));
+
 router.patch('/:contactId', ctrlWrapper(patchContactController));
+
 router.delete('/:contactId', ctrlWrapper(deleteContactController));
 
 export default router;
